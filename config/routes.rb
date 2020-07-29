540e99955c21e888d9f412903b0853b1c6433827
Matheus Fraga <matheus.fraga@playdelivery.com>
2020-07-28 23:12:24 -0300

    Arquivos de rotas configurado

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Define thread de controller, com padrão de formatos em json, só permite subdomínio de 'api', retirando o www.
+  #Especifica que não precisa de caminho a ser digitado na url para entrar nos controllers >> api.site/rota_criada
+  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do 
+
+  end 
 end
