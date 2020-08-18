8aa3ed87cec5481dacd0578e7d6c16c1735e0950
Matheus Fraga <matheus.fraga@playdelivery.com>
2020-08-17 22:38:46 -0300

    symbolize_name add

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe 'Users API', type: :request do
 
 		context 'when the user exists' do 
 			it 'returns the user' do 
-				user_response = JSON.parse(response.body)
-				expect(user_response['id']).to eq(user_id)
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response[:id]).to eq(user_id)
 			end 
 		end 
 
@@ -36,8 +36,8 @@ RSpec.describe 'Users API', type: :request do
 				expect(response).to have_http_status(201)
 			end 
 			it 'returns json data for the created user' do 
-				user_response = JSON.parse(response.body)
-				expect(user_response['email']).to eq(user_params[:email])
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response[:email]).to eq(user_params[:email])
 			end 
 		end 
 
@@ -49,8 +49,8 @@ RSpec.describe 'Users API', type: :request do
 			end
 
 			it 'reutus the JSON data for the erros' do 
-				user_response = JSON.parse(response.body)
-				expect(user_response).to have_key('errors')
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response).to have_key(:errors)
 			end
 		end 
 
