03cf22e785a3ae54e7352c46fa7c01a11baeb936
Matheus Fraga <matheus.fraga@playdelivery.com>
2020-09-23 11:01:22 -0300

    adding authentication for the users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-	# before_action :authenticate_with_token!, only: [:update, :destroy]
+	before_action :authenticate_with_token!, only: [:update, :destroy]
 	respond_to :json
 
 	def show 
@@ -29,7 +29,7 @@ class Api::V1::UsersController < ApplicationController
 	end
 
 	def update 
-		user = User.find(params[:id])
+		user = current_user
 
 		if user.update(user_params)
 			render json: user, status: 200
@@ -40,11 +40,9 @@ class Api::V1::UsersController < ApplicationController
 	end
 
 	def destroy 
-		user = User.find(params[:id])
+		user = current_user.destroy
 
-		if user.destroy
-			head 204
-		end
+		head 204
 	end
 
 	private 
