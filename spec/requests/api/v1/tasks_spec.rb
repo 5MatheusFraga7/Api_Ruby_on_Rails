b10e5a8ac1106825cdcb89f20f0ca221f6646b4b
Matheus Fraga <matheus.fraga@playdelivery.com>
2021-07-12 21:30:20 -0300

    action update criada

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -91,4 +91,50 @@ RSpec.describe 'Task API' do
 						
 		end 
 	end	
+
+	describe 'PUT /task/:id' do 
+
+		let!(:task) { create(:task, user_id: user.id) }
+
+		before do 
+			put "/tasks/#{task.id}", params: { task: task_params }.to_json, headers: headers
+		end
+
+		context 'when the params is valid' do 
+
+			let(:task_params) { { title: 'New Task Title' } }
+
+			it 'status code is 200' do 
+				expect(response).to have_http_status(200)
+			end
+
+			it 'returns the json for updated task' do 
+
+				expect(json_body[:title]).to eq(task_params[:title])
+			end
+
+			it 'updates the task in the database' do 
+				expect( Task.find_by(title: task_params[:title]) ).not_to be_nil
+			end
+		end 
+
+
+		context 'when the params are invalid' do 
+
+			let(:task_params) { { title: '' } }
+
+			it 'status code is 422' do 
+				expect(response).to have_http_status(422)
+			end
+
+			it 'returns the json error for the title' do 
+				expect(json_body[:errors]).to have_key(:title)
+			end
+
+			it 'does not updates the task in the database' do 
+				expect( Task.find_by(title: task_params[:title]) ).to be_nil
+			end
+		end 
+
+	end
 end
