5b559620560c49d37333a980b96ead9ccf6570b8
Matheus Fraga <matheus.fraga@playdelivery.com>
2021-08-09 21:24:00 -0300

    add v2 api

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,20 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #Define thread de controller, com padrão de formatos em json, só permite subdomínio de 'api', retirando o www.
-  #Especifica que não precisa de caminho a ser digitado na url para entrar nos controllers >> api.site/rota_criada
+  # Define thread de controller, com padrão de formatos em json, só permite subdomínio de 'api', retirando o www.
+  # Especifica que não precisa de caminho a ser digitado na url para entrar nos controllers >> api.site/rota_criada
+  # Versão default precisa sempre ficar por ultimo porque o arquivo e rotas é lido de baxo para cima
+
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do 	
-  	namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  	namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
 		resources :users, only: [:show, :create, :update, :destroy]	
 		resources :sessions, only: [:create, :destroy]	
     resources :tasks, only: [:index, :show, :create, :update, :destroy]    
-	
   	end
+    namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
+    resources :users, only: [:show, :create, :update, :destroy] 
+    resources :sessions, only: [:create, :destroy]  
+    resources :tasks, only: [:index, :show, :create, :update, :destroy]    
+    end
   end 
 end
